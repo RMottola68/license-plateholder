8250e6879f40ffb4aa117a8d7b0ec44e51544a77
Ryan Mottola <rmottola68@gmail.com>
2022-08-23 18:01:22 -0400

    small typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ u3 = User.create!( plate: "OFP 857", state: "Indiana", image: "https://prod-ripc
 u4 = User.create!( plate: "SHIBBY", state: "New Jersey", image: "https://media1.popsugar-assets.com/files/thumbor/Iq0Les9n4NgwFsKZw1-spncRxM8/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/19/022/n/44498184/303399cae00254b0_MCDDUWH_FE007/i/Dude-Where-My-Car.jpg")
 
 r1 = Review.create!(title: "Fast and Furious", content: "As quick as they come.", rating: "5")
-r2 = Review.create!(title: "Knight in Shining Charmer/Bae Watch". content: "Me: Standing on the corner of 8th and Evergreen. You: David freaking Hasselhoff", rating: "10")
-r3 = Review.create!(title: "The Limit DOES Exist", content: "You may need a parent trap to catch this mean girl. Met her on a freaky friday while I was herbie fully loaded. Just my luck." rating: "9")
+r2 = Review.create!(title: "Knight in Shining Charmer/Bae Watch", content: "Me: Standing on the corner of 8th and Evergreen. You: David freaking Hasselhoff", rating: "10")
+r3 = Review.create!(title: "The Limit DOES Exist", content: "You may need a parent trap to catch this mean girl. Met her on a freaky friday while I was herbie fully loaded. Just my luck.", rating: "9")
 r4 = Review.create!(title: "...And Then?", content: "You: dude. Your friend: sweet", rating: "420")
 
 ur1 = UserReview.create!(user: u1, review: r1)
