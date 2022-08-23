c7a700531cdb9c2fb025ad21208fa3ad88fa706d
John Hayes <hayesjohn54@yahoo.com>
2022-08-23 16:57:38 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "SEEDING SEEDS..."
+
+u1 = User.create!( plate: "SPD RACR", state: "California")
+u2 = User.create!( plate: "KNIGHT", state: "California")
+u3 = User.create!( plate: "OFP 857", state: "Indiana" )
+u4 = User.create!( plate: "SHIBBY", state: "New Jersey")
+
+r1 = Review.create!(title: "Fast and Furious", content: "As quick as they come.", rating: "5")
+r2 = Review.create!(title: "Knight in Shining Charmer/Bae Watch". content: "Me: Standing on the corner of 8th and Evergreen. You: David freaking Hasselhoff", rating: "10")
+r3 = Review.create!(title: "The Limit DOES Exist", content: "You may need a parent trap to catch this mean girl. Met her on a freaky friday while I was herbie fully loaded. Just my luck." rating: "9")
+r4 = Review.create!(title: "...And Then?", content: "You: dude. Your friend: sweet", rating: "420")
+
+ur1 = UserReview.create!(user: u1, review: r1)
+ur2 = UserReview.create!(user: u2, review: r2)
+ur3 = UserReview.create!(user: u3, review: r3)
+ur4 = UserReview.create!(user: u4, review: r4)
+
+puts "...SEEDS SEEDED "
