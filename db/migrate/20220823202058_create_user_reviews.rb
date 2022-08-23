5f3d80a7870694beed9d020bb4e15318f4d20429
Spencer Mathews <smathews0906@gmail.com>
2022-08-23 15:42:20 -0500

    Fixing migration

--- db/migrate/20220823202058_create_user_reviews.rb
+++ db/migrate/20220823202058_create_user_reviews.rb
@@ -1,8 +1,7 @@
 class CreateUserReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :user_reviews do |t|
-      t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :review, null: false, foreign_key: true
+ 
 
       t.timestamps
     end
