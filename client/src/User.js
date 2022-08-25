0d907b97012f4c34ef5d99df7583bb7001faeedd
Ryan Mottola <rmottola68@gmail.com>
2022-08-25 14:44:05 -0400

    nav / user / usercontainer changes

--- client/src/User.js
+++ client/src/User.js
@@ -1,16 +1,16 @@
 function User({ user }){
     return(
        
-                <div class="card border-success">
-                    <div className="m-5">
-                        <div style={{fontSize: "25px"}}>Plate #: {user.plate}</div>
-                        <div style={{fontSize: "30px"}}>State: {user.state}</div>
-                        <img src={user.image} width="250" height="250" />
-                        <div class="card-button">
-                        <a href="#" class="btn btn-success">Leave Review</a>
-                    </div>
+        <div className="card border-success">
+            <div className="ms-auto mb-1">
+                <div style={{fontSize: "25px"}}>Plate #: {user.plate}</div>
+                <div style={{fontSize: "30px"}}>State: {user.state}</div>
+                <img src={user.image} width="250" height="auto" />
+                <div className="card-button">
+                    <a href="#" className="btn btn-success">Leave Review</a>
                 </div>
             </div>
+        </div>
      
        
     )
--- client/src/UserContainer.js
+++ client/src/UserContainer.js
@@ -35,7 +35,7 @@ function UserContainer({ }) {
    
 
 return(
-    <div className="bg-light border border-5 border-dark justify-contents-center" style={{marginTop: "174px", marginRight: "220px", marginLeft: "220px", borderRadius: "30px"}}>
+    <div className="bg-light border border-5 border-dark justify-contents-center" style={{marginTop: "174px", marginRight: "175px", marginLeft: "175px", borderRadius: "30px"}}>
         <Container>      
             
             <Row >
@@ -48,7 +48,7 @@ return(
                         
                             <Col  xs={3} className="text-dark " key={user.id}>
 
-                                <User className="m-5" user={user} key={user.id} />
+                                <User className="m-5 align-items-center" user={user} key={user.id} />
                                 
                             </Col>
                         )
