840f3e3e791aa539151e1c73ad3a0dd7e0632d12
Ryan Mottola <rmottola68@gmail.com>
2022-08-24 22:52:45 -0400

    small changes to users controller

--- client/src/UserContainer.js
+++ client/src/UserContainer.js
@@ -12,7 +12,7 @@ function UserContainer({ }) {
         fetch('/users')
           .then(res=>res.json())
           .then(data => setUsers(data))
-      },[])
+    },[])
     // console.log(order);
 
     // const renderUsers = users.map((user) => {
@@ -27,7 +27,8 @@ function UserContainer({ }) {
         let userSearchCase = userSearch.toLowerCase()
         return (
             user.plate.toLowerCase().includes(userSearchCase)
-    )})
+        )
+    })
 
     
    
@@ -38,18 +39,19 @@ return(
             
             <Row >
                 <div>
-                    <input type="text" placeholder="Search Plates" className="search" value={userSearch} onChange={(event) => setUserSearch(prevState => prevState = event.target.value)}
- />
+                    <input type="text" placeholder="Search Plates" className="search" value={userSearch} onChange={(event) => setUserSearch(prevState => prevState = event.target.value)} />
                     <button class="btn btn-outline-secondary" type="button">Drive By</button>
                 </div>
-                    {search.map((user) => {return( 
+                    {search.map((user) => {
+                        return( 
                         
-                        <Col  xs={3} className="text-dark " key={user.id}>
+                            <Col  xs={3} className="text-dark " key={user.id}>
 
-                             <User className="m-5" user={user} key={user.id} />
-                             
-                        </Col>
-                                )})} 
+                                <User className="m-5" user={user} key={user.id} />
+                                
+                            </Col>
+                        )
+                    })} 
                                 
                  
             </Row>
