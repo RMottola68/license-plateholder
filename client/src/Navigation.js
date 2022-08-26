4b379bb02b0f05b0c08b8396a46f70878141c6e8
Ryan Mottola <rmottola68@gmail.com>
2022-08-25 20:06:47 -0400

    more nothin

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -56,26 +56,27 @@ function Navigation({ user, setUser }) {
                     <Col className="mt-1 align-items-center" style={{color:"white"}}>
                         <Nav.Item className="">
 
-                            <h2 className="pt-5 d-flex justify-content-center">License Plateholder</h2>
-                            <div className="d-flex justify-content-center">
+                          <h2 className="pt-5 d-flex justify-content-center">License Plateholder</h2>
+                          <div className="d-flex justify-content-center">
 
-                                <Button className="m-3 btn-success" >
-                                    <Link className="text-decoration-none  text-white" to="/profile">Profile</Link>
-                                </Button>
+                            <Button className="m-3 btn-success" >
+                              <Link className="text-decoration-none  text-white" to="users">Search Plates</Link>
+                            </Button>
 
-                                <Button className="m-3 btn-success" >
-                                    <Link className="text-decoration-none  text-white" to="/users">Search Plates</Link>
-                                </Button>
-                            
-                                <Button className="m-3 btn-success" >
-                                    <Link  className="text-decoration-none  text-white" to="/reviews">Review a Plate</Link>
-                                </Button>
+                            <Button className="m-3 btn-success" >
+                              <Link className="text-decoration-none  text-white" to="profile">Profile</Link>
+                                
+                            </Button>
+                        
+                            <Button className="m-3 btn-success" >
+                                <Link  className="text-decoration-none  text-white" to="reviews">Review a Plate</Link>
+                            </Button>
 
 
 
-                              <Button className="m-3 btn-success" >
-                                  <Link  className="text-decoration-none  text-white" to="/newreview">Create new review</Link>
-                              </Button>
+                            <Button className="m-3 btn-success" >
+                                <Link  className="text-decoration-none  text-white" to="newreview">Create new review</Link>
+                            </Button>
                           </div>
                         </Nav.Item>
                     </Col>
